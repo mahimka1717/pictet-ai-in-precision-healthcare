8f8b99998b10191596cb6f223d932801110b127f
mahimka <mahimka@yandex.ru>
2023-04-27 19:34:54 +0400

    fixes from ag

--- src/components/charts/Graph4.js
+++ src/components/charts/Graph4.js
@@ -1,6 +1,6 @@
 import s from '~/src/components/charts/Graph4.module.sass'
 
-import Pie from '~/src/assets/g4pie.svg'
+// import Pie from '~/src/assets/g4pie.svg'
 
 const Graph4 = (p) => {
 
@@ -21,7 +21,41 @@ const Graph4 = (p) => {
             })
         }
         </div>
-        <Pie />
+        {/* <Pie /> */}
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="236" height="209.044" viewBox="0 0 236 209.044">
+            
+            <defs>
+                <clipPath id={`g4mask0`}><path data-ag={`g4mask0`} d="" fill="#ffcc00" /></clipPath>
+                <clipPath id={`g4mask1`}><path data-ag={`g4mask1`} d="" fill="#ffcc00" /></clipPath>
+                <clipPath id={`g4mask2`}><path data-ag={`g4mask2`} d="" fill="#ffcc00" /></clipPath>
+            </defs>    
+               
+            <g clipPath={`url(#g4mask2)`}>
+                <path data-ag="g4pie2" d="M159.578,80.47A79.789,79.789,0,1,1,79.789.681,79.789,79.789,0,0,1,159.578,80.47" transform="translate(0 48.785)" fill="#dfdfdf"/>
+            </g>
+            <g clipPath={`url(#g4mask1)`}>
+                <path data-ag="g4pie1" d="M59.3,79.649V0A73.562,73.562,0,0,1,78.567,2.366Z" transform="translate(22.296 46.089)" fill="#017373"/>
+            </g>
+            <g clipPath={`url(#g4mask0)`}>
+                <path data-ag="g4pie0" d="M141.356,79.635a62.4,62.4,0,1,1-62.4-62.4,62.4,62.4,0,0,1,62.4,62.4" transform="translate(0.922 49.706)" fill="#ffffff"/>
+            </g>
+      
+            <g data-ag="g4pietext0">
+                <text fill="#017373" fontSize="50" fontFamily="LexiconD" letterSpacing="0.003em"><tspan x="109" y="39">&lt;4%</tspan></text>
+            </g>
+            <g data-ag="g4pietext1">
+                <text transform="translate(115 70)" fill="#017373" fontSize="16" fontFamily="LexiconD" letterSpacing="0.002em"><tspan x="0" y="0">Digital Spend</tspan></text>
+            </g>
+
+
+
+            {/* <path data-ag={`g4mask2`} d="" fill="#ffcc00" /> */}
+
+        </svg>
+
+
       </div>
       
     );
--- src/components/initPage.js
+++ src/components/initPage.js
@@ -164,6 +164,29 @@ export function initPage() {
 			const color = document.querySelectorAll(`[data-ag="g3color"]`);
 			const label = document.querySelectorAll(`[data-ag="g3label"]`);
 			const info = document.querySelectorAll(`[data-ag="g3info"]`);
+			const legendItem = document.querySelectorAll(`[data-ag="g3legenditem"]`);
+			const paths = document.querySelectorAll(`[data-ag="g3path"]`);
+			
+			legendItem.forEach((item, i)=>{
+				item.addEventListener("mouseover", (e) => {
+					show(e.target.getAttribute("data-id"))
+				});
+				item.addEventListener("mouseout", (e) => {
+					hide()
+				});
+			})
+
+			const show = function(id){
+				gsap.to(legendItem, {opacity: 0.5, duration: 0.5})
+				gsap.to(paths, {opacity: 0.1, duration: 0.5})
+				gsap.to(legendItem[id], {opacity: 1, duration: 0.5})
+				gsap.to(paths[id], {opacity: 1, duration: 0.5})
+			}
+
+			const hide = function(id){
+				gsap.to(legendItem, {opacity: 1, duration: 0.5})
+				gsap.to(paths, {opacity: 1, duration: 0.5})
+			}
 
 			const off = function(){
 				gsap.killTweensOf([box, mask, color, label, info]);
@@ -195,6 +218,10 @@ export function initPage() {
 				start: ()=>{return "top bottom"},
 				onLeaveBack: ()=>{ off() }
 			});
+
+
+
+
 		},
 
 		graph4: function(){
@@ -211,6 +238,20 @@ export function initPage() {
 			const g4pietext0 = document.querySelectorAll(`[data-ag="g4pietext0"]`);
 			const g4pietext1 = document.querySelectorAll(`[data-ag="g4pietext1"]`);
 
+
+
+			let RAD  = Math.PI / 180;
+			let PI_2 = Math.PI / 2;
+			let arc0 = { start: 0, end: 0, cx: 80, cy: 130, r: 100 };
+			let arc1 = { start: 0, end: 0, cx: 80, cy: 130, r: 100 };
+			let arc2 = { start: 0, end: 0, cx: 80, cy: 130, r: 100 };
+			
+			const cpath0 = document.querySelector(`[data-ag="g4mask0"]`);
+			const cpath1 = document.querySelector(`[data-ag="g4mask1"]`);
+			const cpath2 = document.querySelector(`[data-ag="g4mask2"]`);
+
+
+
 			const off = function(){
 				gsap.killTweensOf([color,val,desc,g4pie0,g4pie1,g4pie2,g4pietext0,g4pietext1]);
 
@@ -218,12 +259,19 @@ export function initPage() {
 				gsap.set(val, {opacity: 0, x: -10});
 				gsap.set(desc, {opacity: 0, y: 0});
 
-				gsap.set(g4pie0, {opacity: 0, scale: 0, transformOrigin: 'center center'});
-				gsap.set(g4pie1, {opacity: 0, scale: 0, transformOrigin: 'center center'});
-				gsap.set(g4pie2, {opacity: 0, scale: 0, transformOrigin: 'center center'});
+				// gsap.set(g4pie0, {opacity: 0, scale: 0, transformOrigin: 'center center'});
+				// gsap.set(g4pie1, {opacity: 0, scale: 0, transformOrigin: 'center center'});
+				// gsap.set(g4pie2, {opacity: 0, scale: 0, transformOrigin: 'center center'});
 
 				gsap.set(g4pietext0, {opacity: 0, x: 10});
 				gsap.set(g4pietext1, {opacity: 0, y: 10});
+
+				cpath0.setAttribute("d", ""); 
+				cpath1.setAttribute("d", ""); 
+				cpath2.setAttribute("d", ""); 
+				gsap.set(arc0, { end: 0 });
+				gsap.set(arc1, { end: 0 });
+				gsap.set(arc2, { end: 0 });
 			}
 			const on = function(){
 				gsap.killTweensOf([color,val,desc,g4pie0,g4pie1,g4pie2,g4pietext0,g4pietext1]);
@@ -232,13 +280,48 @@ export function initPage() {
 				gsap.to(val, {x: 0, opacity: 1, ease: 'power1.out', duration: 0.5, stagger: 0.5, delay: 0})
 				gsap.to(desc, {opacity: 1, y: 0, ease: 'power1.out', duration: 0.5, stagger: 0.5, delay: 0.2})
 				
-				gsap.to(g4pie0, {opacity: 1, scale: 1, ease: 'power1.out', duration: 0.5, delay: 0})
-				gsap.to(g4pie2, {opacity: 1, scale: 1, ease: 'power1.out', duration: 0.5, delay: 0.5})
-				gsap.to(g4pie1, {opacity: 1, scale: 1, ease: 'power1.out', duration: 0.5, delay: 0.75})
+				// gsap.to(g4pie0, {opacity: 1, scale: 1, ease: 'power1.out', duration: 0.5, delay: 0})
+				// gsap.to(g4pie2, {opacity: 1, scale: 1, ease: 'power1.out', duration: 0.5, delay: 0.5})
+				// gsap.to(g4pie1, {opacity: 1, scale: 1, ease: 'power1.out', duration: 0.5, delay: 0.75})
 				
 				gsap.to(g4pietext0, {opacity: 1, x: 0, ease: 'power1.out', duration: 0.5, delay: 1.0})
 				gsap.to(g4pietext1, {opacity: 1, y: 0, ease: 'power1.out', duration: 0.5, delay: 1.2})
+
+
+				gsap.to(arc0, { end: 360, duration: 1.5, ease: "power1.out",  onUpdate: updatePath0, delay: 0.8 });
+				gsap.to(arc1, { end: 25, duration: 1.5, ease: "power1.out",  onUpdate: updatePath1, delay: 0.9 });
+				gsap.to(arc2, { end: 360, duration: 1.5, ease: "power1.out",  onUpdate: updatePath2, delay: 1.0 });
 			}
+
+			function updatePath0() {
+				cpath0.setAttribute("d", getPath(arc0.cx, arc0.cy, arc0.r, arc0.start, arc0.end)); 
+			}
+			function updatePath1() {
+				cpath1.setAttribute("d", getPath(arc1.cx, arc1.cy, arc1.r, arc1.start, arc1.end)); 
+			}
+			function updatePath2() {
+				cpath2.setAttribute("d", getPath(arc2.cx, arc2.cy, arc2.r, arc2.start, arc2.end)); 
+			}
+
+			function getPath(cx, cy, r, a1, a2) {
+				let delta = a2 - a1;
+				if (delta === 360) {	
+					return "M " + (cx - r) + " " + cy + " a " + r + " " + r + " 0 1 0 " + r * 2 + " 0 a " + r + " " + r + " 0 1 0 " + -r * 2 + " 0z"; 
+				}
+				var largeArc = delta > 180 ? 1 : 0;
+				a1 = a1 * RAD - PI_2;
+				a2 = a2 * RAD - PI_2;
+				var x1 = cx + r * Math.cos(a2);   
+				var y1 = cy + r * Math.sin(a2);
+				var x2 = cx + r * Math.cos(a1); 
+				var y2 = cy + r * Math.sin(a1);
+					
+				return "M " + x1 + " " + y1 + " A " + r + " " + r + " 0 " + largeArc + " 0 " + x2 + " " + y2 + " L " + cx + " " + cy + "z";
+			}
+
+			updatePath0()
+			updatePath1()
+			updatePath2()
 
 			off();
 
